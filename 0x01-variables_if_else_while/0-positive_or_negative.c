7d37a3076ec9b0ccd2564bc825331ed047d71245
khalid <khalidboumaghdar07@gmail.com>
2023-07-14 16:57:51 -0700

    khkalid

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ *
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ *
+ * Return: largest number
+ */
+
+int main(int a, int b, int c)
+{
+      for (int n = 1; n <= 10; n++)
+{
+    printf("%d\n", n);
+}	
+}
