21b6318201c08a28e42bc24282b63ec168348f8e
khalid <khalidboumaghdar07@gmail.com>
2023-07-14 17:20:58 -0700

    khkaldddd

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char alp[24] = "cdefghijkltmppqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
