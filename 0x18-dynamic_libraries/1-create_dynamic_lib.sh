1ba94e74b402cefe6606e72043a74daf12d6d045
khalid Boumaghdar <khalidboumaghdar07@gmail.com>
2023-10-16 16:13:32 -0700

    khalid

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -fPIC -c * .c
-gcc -shared *.o -o liball.so
+
+# Compile all the .c files into a dynamic library named liball.so
+gcc -Wall -Werror -Wextra -pedantic -shared -o liball.so *.c
